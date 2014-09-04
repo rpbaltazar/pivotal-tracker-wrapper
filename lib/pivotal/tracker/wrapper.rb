d7990c67311d7094d750b8d2dac72e7e92e07a6b
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-09-04 12:01:54 +0800

    removed unecessary files

--- lib/pivotal/tracker/wrapper.rb
+++
@@ -1,9 +0,0 @@
-require "pivotal/tracker/wrapper/version"
-
-module Pivotal
-  module Tracker
-    module Wrapper
-      # Your code goes here...
-    end
-  end
-end
--- lib/pivotal/tracker/wrapper/version.rb
+++
@@ -1,7 +0,0 @@
-module Pivotal
-  module Tracker
-    module Wrapper
-      VERSION = "0.0.1"
-    end
-  end
-end
