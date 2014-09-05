813f11c88e3de5958b5737a0a2ed896f5041a852
Rui Baltazar <rui.p.baltazar@gmail.com>
2014-09-05 15:39:56 +0800

    Revert "implemented get all projects"
    This reverts commit 870412574a2ba8b2bed1a3ee900e8186a2520c70.

--- spec/pivotal-tracker-wrapper/project_spec.rb
+++ spec/pivotal-tracker-wrapper/project_spec.rb
@@ -21,9 +21,7 @@ describe PivotalTracker::Project do
 
   describe 'API calls' do
     before do
-      VCR.use_cassette 'get-valid-api-token' do
-        PivotalTracker::Client.token(USERNAME, PASSWORD)
-      end
+      PivotalTracker::Client.token(USERNAME, PASSWORD)
     end
 
     describe "#all" do
@@ -39,13 +37,6 @@ describe PivotalTracker::Project do
 
       it "should be a project instance" do
         expect(@projects.first).to be_a(PivotalTracker::Project)
-      end
-
-      it "should contain projects with names and ids" do
-        @projects.each do |proj|
-          expect(proj.valid?).to eq true
-          expect(proj.id).not_to be_nil
-        end
       end
     end
   end
